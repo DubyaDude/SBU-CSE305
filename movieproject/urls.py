ed3333e7a324a90970ed532a7946d545ad0402ae
David Le <david.le@stonybrook.edu>
2018-11-26 15:45:58 -0500

    testing

--- movieproject/urls.py
+++ movieproject/urls.py
@@ -1,4 +1,4 @@
-"""movieproject URL Configuration
+"""movieProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hello.views import myView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/',myView),
 ]
