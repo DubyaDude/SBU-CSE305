9af57d1f52388d0921d6babbf13485f8bedc6978
David Le <david.le@stonybrook.edu>
2018-11-26 18:36:08 -0500

    db change

--- movieproject/settings.py
+++ movieproject/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'movieProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydatabase', 
     }
 }
