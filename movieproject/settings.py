dd64caf1f5ba339f0e3ccdf5c0500afeac62edc7
DubyaDude <xxxshadowcraftxxx@gmail.com>
2018-11-26 17:54:27 -0500

    DB revert

--- movieproject/settings.py
+++ movieproject/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'movieProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ushafiq_cse305',
-        'USER': 'ushafiq',
-        'PASSWORD': 'M3Lt9bzKrt',
-        'HOST': 'cse305.hosted.nfoservers.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase', 
     }
 }
 
